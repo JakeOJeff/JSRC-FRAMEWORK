2f8d5be1c3a97d2184637b35a13861c56fbb80ed
JakeOJeff <87922888+JakeOJeff@users.noreply.github.com>
2022-06-30 17:13:35 +0100

    Changed Color Values into tables
    After this update, you can only add Color values from 0-1 in tables . 
    Before : button.colorR = 50  button.colorG = 50 button.colorB = 50
    Now : button.color = {0.3,0.3,0.3}

--- button.lua
+++ button.lua
@@ -4,7 +4,7 @@ local buttons = {}
 local originalFont = love.graphics.getFont()
 sampleCodeVal = 0
 
-function button:initialize(text, code, widthS, heightS, rx, ry, textColor, font, color1,color2,color3, hoverColor1,hoverColor2,hoverColor3, clickColor1,clickColor2,clickColor3)
+function button:initialize(text, code, widthS, heightS, rx, ry, textColor, font, color, hover, click)
 
 	self.code = code or (function() sampleCode = "hello" 
 	sampleCodeVal = sampleCodeVal + 1
@@ -16,16 +16,10 @@ end)
 	self.ry = ry or 0
 	self.textColor = textColor or {255,255,255}
 	self.font = font or love.graphics.getFont()
-	self.colorR = color1 or 50
-	self.colorG = color2 or 50
-	self.colorB = color3 or 50
-	self.hoverR = hoverColor1 or 45
-	self.hoverG = hoverColor2 or 45
-	self.hoverB = hoverColor3 or 45
-	self.clickR = clickColor1 or 75
-	self.clickG = clickColor2 or 75
-	self.clickB = clickColor3 or 75
-	self.originalColor = {self.colorR,self.colorG,self.colorB} 
+	self.color = color or {0.5,0.5,0.5}
+	self.hover = hover or {0.4,0.4,0.4}
+	self.click = click or {0.7,0.7,0.7}
+	self.Fcolor = {self.color[1],self.color[2],self.color[3]}
 	table.insert(buttons, self)
 	return self
 
@@ -37,22 +31,21 @@ function button:update()
 	if x < self.width + self.font:getWidth(self.text) + 20 and x > self.width and y < self.height + self.font:getHeight(self.text) + 20 and y > self.height then 
 		
 		if not love.mouse.isDown(1) then
-			self.color = {(self.hoverR/255),(self.hoverG/255),(self.hoverB/255)}
-			
-	    elseif love.mouse.isDown(1) then 
+			self.Fcolor = {self.hover[1],self.hover[2],self.hover[3]}
+	    else
 			self.code()
-			self.color = {(self.clickR/255),(self.clickG/255),(self.clickB/255)}
+			self.Fcolor = {self.click[1],self.click[2],self.click[3]}
 			love.timer.sleep(.1)
 		end
 	else
-		self.color = {self.colorR/255,self.colorG/255,self.colorB/255}
+		self.Fcolor = {self.color[1],self.color[2],self.color[3]}
 	end 
 end
 
 function button:draw()
 	love.graphics.setFont(self.font)
 
-	love.graphics.setColor(self.color)
+	love.graphics.setColor(self.Fcolor)
 	love.graphics.rectangle("fill", self.width, self.height, self.font:getWidth(self.text) + 20, self.font:getHeight(self.text) + 20, self.rx, self.ry)
 
 	love.graphics.setColor(self.textColor)
